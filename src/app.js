561a0e25c04ca8de6eee8e0b5af9a9bf77b63afd
SANTHOSH13803 <santhosh13803@gmail.com>
2025-09-04 20:51:05 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.use((req, res, next) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
