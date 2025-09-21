352bed1c3811cee9c652218e52de242aa17bb5de
SANTHOSH13803 <santhosh13803@gmail.com>
2025-09-21 17:54:32 +0530

    saving data in db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const conectDatabase = require("./config/database");
+const User = require("./models/user");
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = {
+    firstName: "Ram",
+    lastName: "Veluru",
+    age: 27,
+    emailId: "RamVeluru@gmail.com",
+    password: "ramv13803"
+  };
+  //  creating a new instance
+  const newUser = new User(user);
+  await newUser.save();
+  res.send("User created");
+});
 
 conectDatabase()
   .then(() => {
